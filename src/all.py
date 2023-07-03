50b8cb51ae9bcf5e31de628b9a6ae57817c2fe83
CoolPlayLin <CoolPlayLin@qq.com>
2023-07-03 13:44:23 +0800

    Update

--- src/all.py
+++ src/all.py
@@ -1,47 +1,5 @@
 from time import sleep
-import requests
-import pathlib
-import os, sys
-
-def komac(path: str, debug: bool = False):
-    Komac = pathlib.Path(path)/"komac.jar"
-    if not debug:
-        with open(Komac, "wb+") as f:
-            file = requests.get("https://gh.api-go.asia/https://github.com/russellbanks/Komac/releases/download/v1.8.0/Komac-1.8.0-all.jar", verify=False)
-            f.write(file.content)
-    return Komac
-
-def command(komac: str, id: str, urls: str, version: str, token: str) -> str:
-    Commands = "java -jar {} update --id {} --urls {} --version {} --submit --token {}".format(komac, id, urls, version, token)
-    return Commands
-
-def clean_string(string: str, keywords: dict[str, str]) -> str:
-    for k in keywords:
-        string = string.replace(k, keywords[k])
-    return string
-
-def str_pop(string: str, index: int)-> str:
-        i = list(string)
-        i.pop(index)
-        i = "".join(i)
-
-        return i
-
-def list_to_str(List: list):
-    new = str(List)
-    new = clean_string(new, {
-         "[": "",
-         "]": "",
-         " ": "",
-         "'": ""
-    })
-    return new
-
-def version_verify(version: str, id: str) -> bool:
-    if len([v for v in requests.get(f"https://winget.vercel.app/api/winget-pkg-versions?pkgid={id}").json()[id] if v == version]) > 0:
-        return False
-    else:
-        return True
+from main import *
 
 def main():
     Commands = []
